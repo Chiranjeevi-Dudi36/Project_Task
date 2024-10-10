53cfc30e155d372c6c82124bdca4a86a8c97891f
Chiranjeevi-Dudi36 <cdudi@anergroup.com>
2024-10-10 20:13:12 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,14 +1,8 @@
 resource "aws_instance" "my_instance" {
   ami           = var.ami
   instance_type = var.instance_type
-  key_name      = aws_key_pair.deployer.key_name
 
   tags = {
     Name = var.name_tag,
   }
-}
-
-resource "aws_key_pair" "deployer" {
-  key_name   = "terra-key"
-  public_key = file("/home/projectvm/Project_Task/terra-key.pub")
 }
